ae2a8f6f7471ef72e6ec06573116e383dd637edb
Noah Zentzis <nzentzis@gmail.com>
2013-06-18 20:43:06 -0700

    Added tests for ADX and SBX

---
+++ tests/adx.asm
@@ -0,0 +1,24 @@
+set b, 1
+set a, 3
+set ex, 4
+adx b, a
+set [1000], a
+set [1001], b
+set [1002], ex
+
+set b, 1
+set a, 3
+adx b, a
+set [1003], a
+set [1004], b
+set [1005], ex
+
+set b, 1
+set a, 3
+adx b, a
+set [1003], a
+set [1004], b
+set [1005], ex
+
+:loop
+set pc, loop
